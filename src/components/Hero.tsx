9c8a0a148dd72242873db3f70302d5a851285601
Mohamed Alaa <arts.MohamedAlaa246093@alexu.edu.eg>
2026-06-24 16:30:14 +0300

    cscs

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -93,7 +93,7 @@ export default function Hero() {
       <div className="absolute inset-0 bg-hero-mesh" />
 
       {/* Noise texture */}
-      <div className="absolute inset-0 opacity-[0.03]"
+      <div className="absolute inset-0 opacity-[0.03]  "
         style={{ backgroundImage: "url(\"data:image/svg+xml,%3Csvg viewBox='0 0 512 512' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='n'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.75' numOctaves='4'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23n)'/%3E%3C/svg%3E\")" }}
       />
 
